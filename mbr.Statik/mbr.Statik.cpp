da4472d92a6b206b4e2d92fe2a8c1d5f623ca951
gheeeeeeee <gheoputra55@gmail.com>
2024-05-13 14:43:42 +0700

    memasukkan akses modifier public dan member serta function didalamny, dan memasukkan meber static dengan variabel nim bertipe integer

--- mbr.Statik/mbr.Statik.cpp
+++ mbr.Statik/mbr.Statik.cpp
@@ -4,7 +4,18 @@ using namespace std;
 
 class mahasiswa
 {
+public:
+    static int nim;
+    int Id;
+    string nama;
 
+    void setID();
+
+    void printAll();
+    mahasiswa(string pnama) :nama(pnama)
+    {
+        setID();
+    }
 };
 
 int main()
