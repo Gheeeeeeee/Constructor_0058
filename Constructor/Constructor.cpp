886065497b3b821272cce607e3c25949097d538f
gheeeeeeee <gheoputra55@gmail.com>
2024-05-13 16:03:13 +0700

    constructor

--- Constructor/Constructor.cpp
+++ Constructor/Constructor.cpp
@@ -1,7 +1,55 @@
 #include <iostream>
 using namespace std;
 
+class Mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    Mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    Mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+    Mahasiswa(string) {
+        string iNama;
+        nama = iNama;
+    }
+    Mahasiswa(int iNim, string iNama)
+    {
+
+        nim = iNim;
+        nama = iNama;
+    }
+    void cetak()
+    {
+    
+
+        cout << "nim  = " << nim << endl;
+        cout << "nama = " << nama << endl;
+        cout << endl;
+    }
+};
+
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Mahasiswa mhs1;
+    Mahasiswa mhs2(20);
+    Mahasiswa mhs3("zhilal spatu");
+    Mahasiswa mhs4(30, "dhonan pempek");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+
 }
