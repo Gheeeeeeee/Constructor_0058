64d5b6a55a2c46af454d30b7af0cc4403d5cf165
gheeeeeeee <gheoputra55@gmail.com>
2024-05-13 15:08:35 +0700

    menambahkan akses modifier dan member didalam class

--- mbrStaticfunc/mbrStaticfunc.cpp
+++ mbrStaticfunc/mbrStaticfunc.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 using namespace std;
 
-class Mahasiswa {
+class Mahasiswa 
+{
+private:
+    static int nim;
 
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; }
+    static int getNim() { return nim; }
+
+    Mahasiswa(string pnama) :nama(pnama)
+    {
+        setID();
+    }
 };
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
 
 
